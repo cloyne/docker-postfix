8db64ca9b54c8c367a528f2dbeb0991d39ccb3db
Mitar <mitar.git@tnode.com>
2014-07-03 01:23:32 -0700

    Initial files for postfix Docker image.

---
+++ run.sh
@@ -0,0 +1,3 @@
+#/bin/bash -e
+
+docker run -d -p 50.0.115.228:25:25/tcp -p 50.0.115.228:465:465/tcp -p 50.0.115.228:587:587/tcp --name mail -h mail.cloyne.net -e MAILNAME=mail.cloyne.net -e MY_NETWORKS='172.17.0.0/16 50.0.115.224/28 127.0.0.0/8' -e ROOT_ALIAS='clonm@bsc.coop,mitar.cloyne@tnode.com' -v /srv/var/log/postfix:/var/log/postfix -v /srv/postfix:/var/spool/postfix cloyne/postfix
